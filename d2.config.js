7458da126cf9fa906ff1ad0baeea60b16b0f5587
Birk Johansson <birk@dhis2.org>
2021-06-18 18:52:51 +0200

    chore(config): update title

--- d2.config.js
+++ d2.config.js
@@ -1,6 +1,7 @@
 const config = {
     type: 'app',
     id: 'b783bf32-0cc2-4d31-aadc-22d4c4807c30',
+    title: 'Simple Example App',
     minDHIS2Version: '2.34',
     entryPoints: {
         app: './src/App.js',
